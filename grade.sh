7cc60a62b28959abd6809b39d97b6487f46260bf
jamesx1205 <56176490+jamesx1205@users.noreply.github.com>
2023-11-07 11:53:37 -0800

    lab

--- grade.sh
+++ grade.sh
@@ -14,3 +14,15 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+files=`find student-submission/*.java`
+for file in $files
+do
+    if [[ -f $file ]] && [[ $file == *ListExamples* ]]
+    then echo 'Correct file is submitted.'
+    else echo 'File NOT submitted correctly!!!'
+    fi 
+done
+
+cp -r student-submission/ListExamples.java grading-area
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore ListExamples
